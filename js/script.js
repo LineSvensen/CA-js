e85aed9820f2f4d13b7010b5b9ab645fed9b4bb7
MSI\lines <line.sven@live.no>
2024-02-08 12:13:41 +0100

    no message

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // const productsInMain = document.getElementById("main");
 // let productsData = []
+
 //
 // fetch("https://api.noroff.dev/api/v1/rainy-days")
 //     .then((response) => response.json())
@@ -13,32 +14,39 @@
 //
 // console.log(productsData);
 
+let productContainer = document.getElementById("product-container");
 
 fetch("https://api.noroff.dev/api/v1/rainy-days")
     .then(response => response.json())
     .then(productsData => {
 
+        productContainer.innerHTML = '';
+
         productsData.forEach(product => {
 
-            const productContainer = document.createElement("section");
-            const titleForProduct = document.createElement("h2");
-            const productDescription = document.createElement("p");
-            const productPrice = document.createElement("p");
-            const productImage = document.createElement("img");
-
-            titleForProduct.textContent = product.title;
-            productDescription.textContent = product.description;
-            productPrice.textContent = `Price: ${product.price}`;
-            productImage.src = product.image;
-            productImage.alt = product.title;
-
-            productContainer.appendChild(titleForProduct);
-            productContainer.appendChild(productDescription);
-            productContainer.appendChild(productPrice);
-            productContainer.appendChild(productImage);
-
-            document.body.appendChild(productContainer);
-
+            if (product.onSale) {
+                productContainer.innerHTML += `
+                <section class="prod-container">
+                    <img class="prod-image" src="${product.image}" alt="${product.title}">
+                    <h2 class="prod-title">${product.title}</h2>
+                    <p class="prod-description">${product.description}</p>
+                    <p class="prod-price-before">Price: ${product.price}</p>
+                    <p class="discount-price">Price: ${product.discountedPrice}</p>
+                    <button>lol</button>
+                </section>
+            `;
+            } else {
+                productContainer.innerHTML += `
+                <section class="prod-container">
+                    <img class="prod-image" src="${product.image}" alt="${product.title}">
+                    <h2 class="prod-title">${product.title}</h2>
+                    <p class="prod-description">${product.description}</p>
+                    <p class="prod-price">Price: ${product.price}</p>
+                    <button>lol</button>
+                </section>
+            `;
+            }
         });
+
     })
     .catch(error => console.error("Error fetching data:", error));
