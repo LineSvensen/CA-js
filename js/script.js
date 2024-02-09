64e9a26099fd8accfe6c424935f736a5e60535ea
MSI\lines <line.sven@live.no>
2024-02-09 12:08:11 +0100

    no message

--- js/script.js
+++ js/script.js
@@ -1,52 +1,96 @@
-// const productsInMain = document.getElementById("main");
-// let productsData = []
-
-//
-// fetch("https://api.noroff.dev/api/v1/rainy-days")
-//     .then((response) => response.json())
-//     .then((productsResultData) => {
-//         productsData = productsResultData;
-//         for (const product of productsData) {
-//
-//         }
-//     })
-//     .catch(error => console.error("Error fetching data:", error));
-//
-// console.log(productsData);
-
 let productContainer = document.getElementById("product-container");
+const buttonsFiltering = document.querySelectorAll(".filterbtn");
 
-fetch("https://api.noroff.dev/api/v1/rainy-days")
-    .then(response => response.json())
-    .then(productsData => {
+buttonsFiltering.forEach(button => {button.addEventListener("click", function(){
+    let filterBtnValue = this.dataset.filter; //
+    if (filterBtnValue === "showall") {
+        getAllProducts();
+    } else {
+        filterProds(filterBtnValue)
+    }
+})})
 
-        productContainer.innerHTML = '';
+function getAllProducts () {
+    fetch("https://api.noroff.dev/api/v1/rainy-days")
+        .then(response => response.json())
+        .then(productsData => {
 
-        productsData.forEach(product => {
+            productContainer.innerHTML = '';
 
-            if (product.onSale) {
-                productContainer.innerHTML += `
+            productsData.forEach(product => {
+
+                if (product.onSale) {
+                    productContainer.innerHTML += `
                 <section class="prod-container">
-                    <img class="prod-image" src="${product.image}" alt="${product.title}">
-                    <h2 class="prod-title">${product.title}</h2>
-                    <p class="prod-description">${product.description}</p>
-                    <p class="prod-price-before">Price: ${product.price}</p>
-                    <p class="discount-price">Price: ${product.discountedPrice}</p>
-                    <button>lol</button>
+                    <div class="card">
+                        <img class="prod-image" src="${product.image}" alt="${product.title}">
+                        <h2 class="prod-title">${product.title}</h2>
+                        <p class="prod-description">${product.description}</p>
+                        <p class="prod-price-before">Price: ${product.price}</p>
+                        <p class="discount-price">Price: ${product.discountedPrice}</p>
+                        <button>lol</button>
+                    </div>    
                 </section>
             `;
-            } else {
-                productContainer.innerHTML += `
+                } else {
+                    productContainer.innerHTML += `
                 <section class="prod-container">
-                    <img class="prod-image" src="${product.image}" alt="${product.title}">
-                    <h2 class="prod-title">${product.title}</h2>
-                    <p class="prod-description">${product.description}</p>
-                    <p class="prod-price">Price: ${product.price}</p>
-                    <button>lol</button>
+                    <div class="card">
+                        <img class="prod-image" src="${product.image}" alt="${product.title}">
+                        <h2 class="prod-title">${product.title}</h2>
+                        <p class="prod-description">${product.description}</p>
+                        <p class="prod-price">Price: ${product.price}</p>
+                        <button>lol</button>
+                    </div>
                 </section>
             `;
-            }
-        });
+                }
+            });
 
-    })
-    .catch(error => console.error("Error fetching data:", error));
+        })
+        .catch(error => console.error("Error fetching data:", error));
+}
+
+getAllProducts();
+
+function filterProds (filterValue)
+{
+    fetch("https://api.noroff.dev/api/v1/rainy-days")
+        .then(response => response.json())
+        .then(productsData => {
+
+            productContainer.innerHTML = '';
+
+            productsData.forEach(product => {
+
+                if (product.onSale && product.gender.toLowerCase() === filterValue) {
+                    productContainer.innerHTML += `
+                <section class="prod-container">
+                    <div class="card">
+                        <img class="prod-image" src="${product.image}" alt="${product.title}">
+                        <h2 class="prod-title">${product.title}</h2>
+                        <p class="prod-description">${product.description}</p>
+                        <p class="prod-price-before">Price: ${product.price}</p>
+                        <p class="discount-price">Price: ${product.discountedPrice}</p>
+                        <button>lol</button>
+                    </div>    
+                </section>
+            `;
+                } else if (product.onSale === false && product.gender.toLowerCase() === filterValue){
+                    productContainer.innerHTML += `
+                <section class="prod-container">
+                    <div class="card">
+                        <img class="prod-image" src="${product.image}" alt="${product.title}">
+                        <h2 class="prod-title">${product.title}</h2>
+                        <p class="prod-description">${product.description}</p>
+                        <p class="prod-price">Price: ${product.price}</p>
+                        <button>lol</button>
+                    </div>
+                </section>
+            `;
+                }
+            });
+
+        })
+        .catch(error => console.error("Error fetching data:", error));
+}
